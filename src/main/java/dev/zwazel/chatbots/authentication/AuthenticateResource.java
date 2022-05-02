2ae5eeaf94ee5fb2c57088e6cf10dc7269593f44
Zwazel <zwazel@hotmail.com>
2022-05-02 08:20:08 +0200

    yoa idk

--- src/main/java/dev/zwazel/chatbots/authentication/AuthenticateResource.java
+++ src/main/java/dev/zwazel/chatbots/authentication/AuthenticateResource.java
@@ -1,20 +1,19 @@
 package dev.zwazel.chatbots.authentication;
 
-import dev.zwazel.chatbots.classes.enums.UserRole;
-import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.core.SecurityContext;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.NewCookie;
+import jakarta.ws.rs.core.Response;
 
 @Path("/auth")
 public class AuthenticateResource {
-    @Inject
-    private SecurityContext securityContext;
-
     @GET
     @Path("/login")
-    public String login() {
-        return "You're now logged in!";
+    @Produces("application/json")
+    public Response login() {
+        return Response.ok("{\"message\": \"You're now logged in!\"}").cookie(
+                new NewCookie("auth", "true")).build();
     }
 
     @GET
@@ -26,16 +25,12 @@ public class AuthenticateResource {
     @GET
     @Path("auth-check")
     public String authCheck() {
-        return "You're authenticated!";
+        return "idk if you're authenticated";
     }
 
     @GET
     @Path("/admin-check")
     public String loginCheck() {
-        if (securityContext.isUserInRole(UserRole.ADMIN.toString())) {
-            return "You're an Admin!";
-        } else {
-            return "You're not an Admin!";
-        }
+        return "idk if you're an admin";
     }
 }

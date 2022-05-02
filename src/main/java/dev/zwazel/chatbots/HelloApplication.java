49d1e45b73808cba97f6b812f15ea4e9d62246cb
Zwazel <zwazel@hotmail.com>
2022-05-02 08:51:19 +0200

    it no work

--- src/main/java/dev/zwazel/chatbots/HelloApplication.java
+++ src/main/java/dev/zwazel/chatbots/HelloApplication.java
@@ -1,9 +1,13 @@
 package dev.zwazel.chatbots;
 
+import jakarta.annotation.security.DeclareRoles;
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
 @ApplicationPath("/api")
+@ApplicationScoped
+@DeclareRoles({"user", "admin"})
 public class HelloApplication extends Application {
 
 }

0f124197a56f8c23a98614694c5d8f5431bc2fd5
Zwazel <zwazel@hotmail.com>
2022-05-10 16:24:22 +0200

    fetch!

--- src/main/java/dev/zwazel/chatbots/HelloResource.java
+++ src/main/java/dev/zwazel/chatbots/HelloResource.java
@@ -3,12 +3,17 @@ package dev.zwazel.chatbots;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Response;
 
 @Path("/hello-world")
 public class HelloResource {
     @GET
     @Produces("text/plain")
-    public String hello() {
-        return "Hello, World!";
+    public Response hello() {
+        return Response
+                .status(200)
+                .header("Access-Control-Allow-Origin", "*")
+                .entity("Hello, World!")
+                .build();
     }
 }

048637d666777cc57abb3ac89a43c26065aaead4
Zwazel <zwazel@hotmail.com>
2022-05-12 11:44:49 +0200

    it should be working on heroku now!

--- src/main/java/dev/zwazel/chatbots/classes/model/User.java
+++ src/main/java/dev/zwazel/chatbots/classes/model/User.java
@@ -1,5 +1,10 @@
 package dev.zwazel.chatbots.classes.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class User {
     private String id;
 
@@ -23,21 +28,5 @@ public class User {
                 "\"id\":\"" + id + "\"," +
                 "\"username\":\"" + name + "\"" +
                 "}";
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }

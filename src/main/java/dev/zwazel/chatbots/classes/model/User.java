a8fea7e6f600ffa6a58c7ce69c8bdf9056573689
Zwazel <zwazel@hotmail.com>
2022-05-14 18:28:40 +0200

    am working on java docs

--- src/main/java/dev/zwazel/chatbots/classes/model/User.java
+++ src/main/java/dev/zwazel/chatbots/classes/model/User.java
@@ -3,18 +3,49 @@ package dev.zwazel.chatbots.classes.model;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * User class
+ *
+ * @author Zwazel
+ * @version 0.1
+ * @since 0.1
+ */
 @Setter
 @Getter
 public class User {
+    /**
+     * User id
+     *
+     * @since 0.1
+     */
     private String id;
 
+    /**
+     * User name
+     *
+     * @since 0.1
+     */
     private String name;
 
+    /**
+     * User constructor with id and name
+     *
+     * @param id   user id
+     * @param name name of user
+     * @author Zwazel
+     * @since 0.1
+     */
     public User(String id, String name) {
         this.id = id;
         this.name = name;
     }
 
+    /**
+     * User to String method
+     *
+     * @author Zwazel
+     * @since 0.1
+     */
     @Override
     public String toString() {
         return "User{" +
@@ -23,6 +54,12 @@ public class User {
                 '}';
     }
 
+    /**
+     * User toJson method
+     *
+     * @author Zwazel
+     * @since 0.1
+     */
     public String toJson() {
         return "{" +
                 "\"id\":\"" + id + "\"," +
--- src/main/java/dev/zwazel/chatbots/configs/CorsFilter.java
+++ src/main/java/dev/zwazel/chatbots/configs/CorsFilter.java
@@ -7,9 +7,25 @@ import jakarta.ws.rs.ext.Provider;
 
 import java.io.IOException;
 
+/**
+ * Filter class for allowing authorization with CORS
+ * <br>
+ * refer to <a href="https://www.baeldung.com/cors-in-jax-rs#1-using-the-filter">Baeldung.com</a>
+ *
+ * @version 1.0
+ * @since 0.1
+ */
 @Provider
 public class CorsFilter implements ContainerResponseFilter {
 
+    /**
+     * Sets the response headers to allow CORS
+     *
+     * @param requestContext  the request context
+     * @param responseContext the response context
+     * @throws IOException if there is an error
+     * @since 0.1
+     */
     @Override
     public void filter(ContainerRequestContext requestContext,
                        ContainerResponseContext responseContext) throws IOException {

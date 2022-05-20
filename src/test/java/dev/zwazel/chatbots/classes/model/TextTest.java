475a450d3b78c46e3b7df32171921921ada9b09c
Zwazel <zwazel@hotmail.com>
2022-05-20 16:18:40 +0200

    oke it does.

--- src/test/java/dev/zwazel/chatbots/classes/model/TextTest.java
+++ src/test/java/dev/zwazel/chatbots/classes/model/TextTest.java
@@ -2,12 +2,11 @@ package dev.zwazel.chatbots.classes.model;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TextTest {
     @Test
-    void testTextNeeded() {
+    public void testTextNeeded() {
         assertThrows(NullPointerException.class,
                 () -> Text.builder()
                         .build()
@@ -15,7 +14,7 @@ public class TextTest {
     }
 
     @Test
-    void testSuccessfulBuild() {
+    public void testSuccessfulBuild() {
         Text text = Text.builder().text("text").build();
 
         assertEquals(0, text.getAmountUsed());
@@ -23,7 +22,7 @@ public class TextTest {
     }
 
     @Test
-    void testSuccessfulBuildWithAmountUsed() {
+    public void testSuccessfulBuildWithAmountUsed() {
         Text text = Text.builder().text("text").amountUsed(1).build();
 
         assertEquals(1, text.getAmountUsed());
@@ -31,10 +30,10 @@ public class TextTest {
     }
 
     @Test
-    void testNoArgConstructor() {
+    public void testNoArgConstructor() {
         Text text = new Text();
 
         assertEquals(0, text.getAmountUsed());
-        assertEquals(null, text.getText());
+        assertNull(text.getText());
     }
 }

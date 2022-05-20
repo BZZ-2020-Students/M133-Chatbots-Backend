037d752e70b389cc2ef1384995abe2742cdca428
Zwazel <zwazel@hotmail.com>
2022-05-20 16:17:21 +0200

    does that make a difference?

--- src/test/java/dev/zwazel/chatbots/classes/model/TextTest.java
+++ src/test/java/dev/zwazel/chatbots/classes/model/TextTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-class TextTest {
+public class TextTest {
     @Test
     void testTextNeeded() {
         assertThrows(NullPointerException.class,

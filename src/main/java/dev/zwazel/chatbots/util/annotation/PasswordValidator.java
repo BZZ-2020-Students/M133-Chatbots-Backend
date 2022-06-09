78d72841d91bb7377d2c1dd8ade0327cadc0399d
Zwazel <zwazel@hotmail.com>
2022-06-09 16:15:09 +0200

    more special characters are now valid for passwords

--- src/main/java/dev/zwazel/chatbots/util/annotation/PasswordValidator.java
+++ src/main/java/dev/zwazel/chatbots/util/annotation/PasswordValidator.java
@@ -12,8 +12,11 @@ public class PasswordValidator implements ConstraintValidator<Password, String> 
             return false;
         }
 
+
         // Checks for min and max characters, at least one uppercase letter, one lowercase letter, one number and one special character:
-        return value.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{" +
+        String specialChars = "!@#$%^&*()_+{}|:\"<>?`~\\-=\\[\\];',./";
+        return value.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[" + specialChars + "])" +
+                "[A-Za-z\\d" + specialChars + "]{" +
                 Constants.MIN_PASSWORD_LENGTH + "," +
                 Constants.MAX_PASSWORD_LENGTH + "}$");
     }

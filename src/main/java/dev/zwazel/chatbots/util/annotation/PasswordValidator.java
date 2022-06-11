7b3c12695c6dcebca96e12cbf6905d369c317b84
Zwazel <zwazel@hotmail.com>
2022-06-11 23:32:41 +0200

    added some special chars because why not

--- src/main/java/dev/zwazel/chatbots/util/annotation/PasswordValidator.java
+++ src/main/java/dev/zwazel/chatbots/util/annotation/PasswordValidator.java
@@ -12,9 +12,8 @@ public class PasswordValidator implements ConstraintValidator<Password, String> 
             return false;
         }
 
-
         // Checks for min and max characters, at least one uppercase letter, one lowercase letter, one number and one special character:
-        String specialChars = "!@#$%^&*()_+{}|:\"<>?`~\\-=\\[\\];',./";
+        String specialChars = "!@#$%^&*()_+{}|:\"<>?`~\\-=\\[\\];',./äöüèàéìòùÀÉÌÒÙ";
         return value.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[" + specialChars + "])" +
                 "[A-Za-z\\d" + specialChars + "]{" +
                 Constants.MIN_PASSWORD_LENGTH + "," +
